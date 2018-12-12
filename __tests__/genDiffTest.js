7b570856c1b03d2cc996fc3e1961b551992212a9
Vasily Varakin <bukawandr@gmail.com>
2018-12-12 08:57:58 +0300

    corrected

--- __tests__/genDiffTest.js
+++ __tests__/genDiffTest.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import genDiff from '../src';
 
 
-const actual = genDiff('__tests__/__fixtures__/before.json',
-  '__tests__/__fixtures__/after.json');
-const expected = fs.readFileSync('__tests__/__fixtures__/answer.txt', 'utf8');
-
-
-test('genDiff vs answer', () => expect(actual).toBe(expected.trim()));
+test('genDiff vs answer', () => {
+  const actual = genDiff('__tests__/__fixtures__/before.json',
+    '__tests__/__fixtures__/after.json');
+  const expected = fs.readFileSync('__tests__/__fixtures__/answer.txt', 'utf8').trim();
+  expect(actual).toBe(expected);
+});
