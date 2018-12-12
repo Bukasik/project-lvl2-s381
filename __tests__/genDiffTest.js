51a897435cefb7caebefbea499c82ddda79ef74f
Vasily Varakin <bukawandr@gmail.com>
2018-12-12 13:02:26 +0300

    step 4

--- __tests__/genDiffTest.js
+++ __tests__/genDiffTest.js
@@ -8,9 +8,17 @@ test('genDiff JSON vs answer', () => {
   const expected = fs.readFileSync('__tests__/__fixtures__/answer.txt', 'utf8').trim();
   expect(actual).toBe(expected);
 });
+
 test('genDiff YAML vs answer', () => {
   const actual = genDiff('__tests__/__fixtures__/before.yml',
     '__tests__/__fixtures__/after.yml');
+  const expected = fs.readFileSync('__tests__/__fixtures__/answer.txt', 'utf8').trim();
+  expect(actual).toBe(expected);
+});
+
+test('genDiff INI vs answer', () => {
+  const actual = genDiff('__tests__/__fixtures__/before.ini',
+    '__tests__/__fixtures__/after.ini');
   const expected = fs.readFileSync('__tests__/__fixtures__/answer.txt', 'utf8').trim();
   expect(actual).toBe(expected);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,11 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parser = (data, extName) => {
   const mapping = {
     '.json': JSON.parse,
     '.yml': yaml.safeLoad,
+    '.ini': ini.parse,
   };
   return mapping[extName](data);
 };
